01a994efa2a34b032eaaac1c7283748ea5c5afa9
Alexander Forsch <alexander.forsch@bytesatwork.ch>
2024-02-09 15:31:47 +0100

    conf.py: Update copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'byteWIKI'
-copyright = '2023, bytes at work AG'
+copyright = '2024, bytes at work AG'
 author = 'Johannes Böhm and others'
 
 # The full version, including alpha/beta/rc tags
